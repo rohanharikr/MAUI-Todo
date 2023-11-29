d0b342389aeaf7c411bb3f1d7c01e22d0563164d
rohanharikr <rohanharikumar80@gmail.com>
2023-11-29 23:48:21 +0530

    start of ui tests

---
+++ Tests/UI/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace UITests;
+
+public abstract class BaseTest
+{
+    protected AppiumDriver App => AppiumSetup.App;
+
+    protected AppiumElement FindUIElement(string id)
+    {
+        if (App is WindowsDriver)
+        {
+            return App.FindElement(MobileBy.AccessibilityId(id));
+        }
+
+        return App.FindElement(MobileBy.Id(id));
+    }
+
+    protected List<AppiumElement> FindUIElements(string id)
+    {
+        if (App is WindowsDriver)
+        {
+            return App.FindElements(MobileBy.AccessibilityId(id)).ToList();
+        }
+
+        return App.FindElements(MobileBy.Id(id)).ToList();
+    }
+}
---
+++ Tests/UI/MainPage.test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace UITests;
+
+public class MainPageTest : BaseTest
+{
+    [Test]
+    public void Add_Todo()
+    {
+        var todoEntry = FindUIElement("TodoEntry");
+        var addTodoButton = FindUIElement("AddTodoButton");
+
+        todoEntry.SendKeys("Hello World");
+        addTodoButton.Click();
+
+        var testing = FindUIElements("TodoItem");
+        Assert.That(testing[0].Text, Is.EqualTo("Hello World"));
+    }
+}
